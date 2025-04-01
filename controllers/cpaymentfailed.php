de3f5264ef96f5044e1335a862eb82b39cc9dfbb
daVinciManuel <mgmt.manuel02@gmail.com>
2025-04-01 20:47:48 +0200

    Add payment failed files

---
+++ controllers/cpaymentfailed.php
@@ -0,0 +1,3 @@
+<?php
+$msg = 'Something went wrong :( </br> Please try again later.';
+require_once './view/vpaymentfailed.php';
---
+++ view/vpaymentfailed.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5,url=./index.php">
+    <title>PORTAL MUSICAL</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+  </head>
+  <body class="bg-dark">
+    <header>
+      <a class="btn btn-danger" href="./controllers/login/logout.php">Logout</a>
+    </header>
+    <main>
+    <h1><?php echo $msg; ?></h1>
+    </main>
+  </body>
+</html>
